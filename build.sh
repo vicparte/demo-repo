ce9aa06d1243d06664b7bbbbb08cbc695d04e04a
Vicky <vicparte07@gmail.com>
2023-11-07 22:38:45 +0530

    changes made to build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
-docker build -t vicparte/dev-demo:latest .
+# Define the image name and version file
 
-###end of file#####
+image_name="vicparte/dev-demo"
+version_file="version.txt"
+
+
+if [ ! -f $version_file ]; then
+    echo "1" > $version_file
+fi
+
+# Read the current version from the file
+current_version=$(cat $version_file)
+
+# Increment the version number
+new_version=$((current_version + 1))
+
+# Build the Docker image with the specified tag
+docker build -t $image_name:$new_version .
+
+# Push the image to Docker Hub
+docker push $image_name:$new_version
+
+# Update the version file with the new version
+echo $new_version > $version_file
