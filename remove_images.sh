35f71617b568c1e85750325103cc2144f534aa90
Vicky <vicparte07@gmail.com>
2023-11-09 01:46:49 +0530

    added remove.sh

--- remove_images.sh
+++ remove_images.sh
@@ -2,10 +2,10 @@
 
 ##tag images##
 
-docker tag react_image vicparte/dev-react_image
-docker push	vicparte/dev-react_image
+docker tag react_image vicparte/dev-react_image:v1
+docker push	vicparte/dev-react_image:v1
 
 
 ##remove images##
 docker rmi -f react_image
-docker rmi -f vicparte/dev-react_image
+docker rmi -f vicparte/dev-react_image:v1
