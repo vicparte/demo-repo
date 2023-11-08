0357a66f5b9da7238e5e2b4b3762bb98898d11f6
Vicky <vicparte07@gmail.com>
2023-11-09 00:01:58 +0530

    some files editied

--- build.sh
+++ build.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-# Below line define name and version
 
-image_name="vicparte/dev-react_image"
-version_file="version.txt"
+docker build -t react_image .
 
-
-if [ ! -f $version_file ]; then
-    echo "1" > $version_file
-fi
-
-# Read the current version from the file
-current_version=$(cat $version_file)
-
-# Increment the version number
-new_version=$((current_version + 1))
-
-# Build the Docker image with the specified tag
-docker build -t $image_name:$new_version .
-
-# Push the image to Docker Hub
-docker push $image_name:$new_version
-
-# Update the version file with the new version
-echo $new_version > $version_file
